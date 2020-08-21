4df106514c3c009cf03ea0ae9ac593a4a62fba82
Lisa Hou <lqnhou@gmail.com>
2020-08-20 17:30:31 -0700

    implemented linkedlist and started debugging tree

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -1,15 +1,41 @@
 var LinkedList = function() {
   var list = {};
-  list.head = null;
-  list.tail = null;
+  list.head = null; //refers to the head node
+  list.tail = null; //refers to the tail node
 
   list.addToTail = function(value) {
+    let currentTail = list.tail;//currentTail = null
+    let newNode = Node(value); //node(4)
+    if (currentTail === null) {
+      list.head = newNode;
+      list.tail = newNode;
+    } else {
+      currentTail.next = newNode; // null.next = node(4)
+      list.tail = newNode;
+    }
   };
 
   list.removeHead = function() {
+    let temp = list.head; //get the current head node
+    list.head = temp.next; //set head to next node
+    return temp.value; //return prev head
+
   };
 
   list.contains = function(target) {
+    //check all nodes before tail node
+    let pointer = list.head;
+    while (pointer.next !== null) {
+      if (pointer.value === target) {
+        return true;
+      }
+      pointer = pointer.next;
+    //check tail node separately
+    } if (pointer.value === target) {
+      return true;
+    } else {
+      return false;
+    }
   };
 
   return list;
@@ -17,10 +43,8 @@ var LinkedList = function() {
 
 var Node = function(value) {
   var node = {};
-
   node.value = value;
   node.next = null;
-
   return node;
 };
 
--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -3,17 +3,31 @@ var Tree = function(value) {
   newTree.value = value;
 
   // your code here
-  newTree.children = null;  // fix me
+  newTree.children = []; //array containing a number of subtrees
 
+  _.extend(newTree, treeMethods);
   return newTree;
 };
 
 var treeMethods = {};
 
+//takes any value, sets that as the target of a node, and adds that node as a child of the tree
 treeMethods.addChild = function(value) {
+  let newTree = Tree(value);
+  this.children.push(newTree);
 };
 
+//takes any input and returns a boolean reflecting whether it can be found as the value of the target node or any descendant node
 treeMethods.contains = function(target) {
+  if (this.value === target) {
+    return true;
+  }
+  for (var i = 0; i < this.children.length; i++) {
+    if (this.children[i].treeMethods.contains() === true) {
+      return true;
+    }
+  }
+  return false;
 };
 
 
